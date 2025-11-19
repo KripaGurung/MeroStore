330d614c1ae63ccdcc85138a5ee803491b577d9e
KripaGurung <krepagurung@gmail.com>
2025-11-19 11:28:47 +0545

    added simple css in featureproduct componenet

--- MeroStore/src/component/FeaturedProduct.jsx
+++ MeroStore/src/component/FeaturedProduct.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
+import "./Featuredproduct.css";
 
 const FeaturedProduct = () => {
     const [products, setProducts] = useState([]);
-    
+
     useEffect(() => {
         fetch("https://fakestoreapi.com/products")
-        .then((response) => response.json())
-        .then((data) => setProducts(data))
+            .then((response) => response.json())
+            .then((data) => setProducts(data));
     }, []);
-    
+
     const handleAddToCart = (product) => {
         let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
@@ -18,31 +19,33 @@ const FeaturedProduct = () => {
             alert(`${product.title} added to cart!`);
         }
     };
-    
+
     const isInCart = (id) => {
         const cart = JSON.parse(localStorage.getItem("cart")) || [];
         return cart.some((item) => item.id === id);
     };
-    
+
     return (
         <div className="mainContainer">
 
             <h1 className="Heading">Featured Product</h1>
             <p className="secondHeading">Check out our featured products!</p>
-            
-            <div className="productContainer">
 
+            <div className="productContainer">
                 {products.slice(0, 8).map((product) => (
                     <div key={product.id} className="cardContainer">
-                        <img src={product.image} alt={product.title} className="imageContainer"/>
-                    
-                        <div className="infoContainer">
 
+                        <div className="imageWrapper">
+                            <img src={product.image} alt={product.title} />
+                        </div>
+
+                        <div className="infoContainer">
                             <p className="productTitle">{product.title}</p>
                             <p className="productPrice">${product.price}</p>
+
                             <button onClick={() => handleAddToCart(product)} disabled={isInCart(product.id)} className={isInCart(product.id) ? "btn addedBtn" : "btn addBtn"}>{isInCart(product.id) ? "Added" : "Add to cart"}</button>
-                        
                         </div>
+
                     </div>
                 ))}
             </div>
