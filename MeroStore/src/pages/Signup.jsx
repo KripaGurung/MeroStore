ff816cd1717c7194a90256e7ca791ccb308a0a46
KripaGurung <krepagurung@gmail.com>
2025-11-20 13:33:05 +0545

    added css in login page

--- MeroStore/src/pages/Signup.jsx
+++ MeroStore/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import {React, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import "./signup.css"
 
 const Register = () => {
 
@@ -44,25 +45,26 @@ const Register = () => {
     };
 
     return(
-        <div className="">
-            <div className="">
+        <div className="registerBox">
+            <div className="registerContainer">
                 <h2>Registration Form</h2>
                 <form  onSubmit={handleSubmit} id="registerForm">
 
                     <label htmlFor="fullName">Full Name</label>
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="fullName" name="fullName" placeholder="Enter Your Name" required />
 
-                    <label htmlFor="email" className="mb-1 text-gray-600 font-medium">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" placeholder="Enter Your Email" required />
 
-                    <label htmlFor="password" className="mb-1 text-gray-600 font-medium">Password</label>
+                    <label htmlFor="password">Password</label>
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="password" name="password" placeholder="Enter Your Password" required />
 
-                    <label htmlFor="confirmPassword" className="mb-1 text-gray-600 font-medium">Confirm Password</label>
+                    <label htmlFor="confirmPassword">Confirm Password</label>
                     <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} id="confirmPassword" name="confirmPassword" placeholder="Confirm Your Password" required />
                     
-                    <div>
-                        <button  type="submit">Sign Up</button>
+                    <button className="signBtn">Sign Up</button>
+                    
+                    <div className="loginLink">
                         <p>Already have an account? <a href="/">Login</a></p>
                     </div>
                 </form>
