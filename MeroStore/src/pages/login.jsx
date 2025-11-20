b30964dba5b0a9aca3b8b05db50db94110d85f39
KripaGurung <krepagurung@gmail.com>
2025-11-20 11:33:09 +0545

    modify the login code and added css in login

--- MeroStore/src/pages/login.jsx
+++ MeroStore/src/pages/login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import "./login.css";
 
 const Login = () => {
 
@@ -42,23 +43,26 @@ const Login = () => {
     };
 
     return(
-          <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-            <div className="bg-white p-10 rounded-2xl shadow-lg w-full max-w-md transform transition duration-300 hover:-translate-y-1 hover:shadow-2xl">
-                <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Welcome Back to Your Account</h2>
-                <form onSubmit={handleSubmit} id="loginForm" className="flex flex-col">
+        <div className="mainBox">
+            <div className="formContainer">
+                <h2>Welcome Back to Your Account</h2>
+                <form onSubmit={handleSubmit} id="loginForm">
                     
-                    <label htmlFor="email" className="mb-1 text-gray-600 font-medium">Email</label>
-                    <input type="email"  value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" placeholder="Enter Your Email" required className="mb-4 px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-green-400"/>  
+                    <label htmlFor="email">Email</label>
+                    <input type="email"  value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" placeholder="Enter Your Email" required/>  
 
-                    <label htmlFor="password" className="mb-1 text-gray-600 font-medium">Password</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="password" name="password" placeholder="Enter Your Password" required className="mb-4 px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-green-400"/>
-
-                     <button className="bg-[#2e7d32] text-white font-semibold py-3 px-6 rounded-lg hover:bg-[#27632a] transition duration-300">Login</button>
-                    <p className="text-center text-gray-500 mt-4">Don't have an account? <a href="/signup" className="text-blue-500 ml-1 hover:text-blue-600 font-medium">Sign Up</a></p>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="password" name="password" placeholder="Enter Your Password" required />
+                    
+                    <button>Login</button>
+                    
+                    <div className="signupLink">
+                        <p>Don't have an account? <a href="/signup">Sign Up</a></p>
+                    </div>
 
                 </form>
             </div>
-          </div>
+        </div>
     );
 }
 
