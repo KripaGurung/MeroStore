04ece39a5cb6a4e4cd5cbd7df8d85c176cc1275f
KripaGurung <krepagurung@gmail.com>
2025-11-30 11:58:34 +0545

    Add toast success message in feature product section

--- MeroStore/src/component/FeaturedProduct.jsx
+++ MeroStore/src/component/FeaturedProduct.jsx
@@ -1,69 +1,3 @@
-// import React from "react";
-// import { useEffect } from "react";
-// import "./Featuredproduct.css";
-// import axios from "axios";
-// import { useSelector, useDispatch } from "react-redux";
-// import { addToCart } from "../redux/slices/cartSlice";
-// import { setLoading, setProducts, setError } from "../redux/slices/productSlice";
-
-
-// const FeaturedProduct = () => {
-//   const dispatch = useDispatch();
-
-//   const loading = useSelector((state) => state.product.loading);
-//   const products = useSelector((state) => state.product.products);
-//   const cartItems = useSelector((state) => state.cart.items);
-
-//   const isInCart = (id) => cartItems.some((item) => item.id === id);
-
-//   const handleAddToCart = (product) => {
-//     dispatch(addToCart(product));
-//     alert(`${product.title} added to cart!`);
-//   };
-
-//    useEffect(() => {
-//     if (products.length === 0) { dispatch(setLoading());
-      
-//       axios.get("https://fakestoreapi.com/products")
-//         .then((res) => dispatch(setProducts(res.data)))
-//         .catch((err) => {
-//           console.log(err);
-//           dispatch(setError("Failed to load products"));
-//         });
-//     }
-//   }, [products, dispatch]);
-
-//   if (loading) {
-//     return <h2 className="loading-text">Loading featured products...</h2>;
-//   }
-
-//   return (
-//     <div className="main-container">
-//       <h1 className="Heading">Featured Product</h1>
-//       <p className="second-heading">Check out our featured products!</p>
-
-//       <div className="product-container">
-//         {products.slice(0, 8).map((product) => (
-//           <div key={product.id} className="card-container">
-//             <div className="image-wrapper">
-//               <img src={product.image} alt={product.title} />
-//             </div>
-
-//             <div className="info-container">
-//               <p className="product-title">{product.title}</p>
-//               <p className="product-price">${product.price}</p>
-
-//               <button onClick={() => handleAddToCart(product)} disabled={isInCart(product.id)} className={isInCart(product.id) ? "btn added-btn" : "btn add-btn"}>{isInCart(product.id) ? "Added" : "Add to cart"}</button>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FeaturedProduct;
-
 import React, { useEffect } from "react";
 import "./Featuredproduct.css";
 import axios from "axios";
@@ -71,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../redux/slices/cartSlice";
 import { setLoading, setProducts, setError } from "../redux/slices/productSlice";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const FeaturedProduct = () => {
   const dispatch = useDispatch();
@@ -83,7 +18,7 @@ const FeaturedProduct = () => {
 
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
-    alert(`${product.title} added to cart!`);
+    toast.success(`${product.title} added to cart!`);
   };
 
   useEffect(() => {
